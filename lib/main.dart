6b4d7a23c18a3df98531a5473d03988f90d1216e
kjm0202 <kjm0202@empas.com>
2025-08-06 18:17:23 +0900

    refactor: remove Provider dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'theme.dart';
 import 'portfolio_page.dart';
 
@@ -8,25 +7,51 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+  final String _themeKey = 'theme_mode';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadThemeFromPrefs();
+  }
+
+  Future<void> _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isDarkMode = prefs.getBool(_themeKey) ?? false;
+    });
+  }
+
+  Future<void> _saveThemeToPrefs(bool isDarkMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, isDarkMode);
+  }
+
+  void toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+    _saveThemeToPrefs(_isDarkMode);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ThemeProvider(),
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, _) {
-          return MaterialApp(
-            title: 'Portfolio',
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-            home: const PortfolioPage(),
-          );
-        },
-      ),
+    return MaterialApp(
+      title: 'Portfolio',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: PortfolioPage(isDarkMode: _isDarkMode, onThemeToggle: toggleTheme),
     );
   }
 }
--- lib/theme_toggle.dart
+++ lib/theme_toggle.dart
@@ -1,44 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'theme_provider.dart';
 
 class ThemeToggle extends StatelessWidget {
-  const ThemeToggle({Key? key}) : super(key: key);
+  final bool isDarkMode;
+  final VoidCallback onToggle;
+
+  const ThemeToggle({
+    super.key,
+    required this.isDarkMode,
+    required this.onToggle,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
-        return AnimatedSwitcher(
-          duration: const Duration(milliseconds: 300),
-          transitionBuilder: (Widget child, Animation<double> animation) {
-            return ScaleTransition(scale: animation, child: child);
-          },
-          child: Switch(
-            key: ValueKey<bool>(themeProvider.isDarkMode),
-            value: themeProvider.isDarkMode,
-            onChanged: (_) => themeProvider.toggleTheme(),
-            activeColor: Theme.of(context).colorScheme.primary,
-            activeTrackColor: Theme.of(context).colorScheme.primary.withOpacity(0.5),
-            inactiveThumbColor: Theme.of(context).colorScheme.secondary,
-            inactiveTrackColor: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
-            thumbIcon: MaterialStateProperty.resolveWith<Icon?>((states) {
-              if (states.contains(MaterialState.selected)) {
-                return Icon(
-                  Icons.dark_mode,
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  size: 16,
-                );
-              }
-              return Icon(
-                Icons.light_mode,
-                color: Theme.of(context).colorScheme.onSecondary,
-                size: 16,
-              );
-            }),
-          ),
-        );
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return ScaleTransition(scale: animation, child: child);
       },
+      child: Switch(
+        key: ValueKey<bool>(isDarkMode),
+        value: isDarkMode,
+        onChanged: (_) => onToggle(),
+        activeColor: Theme.of(context).colorScheme.primary,
+        activeTrackColor: Theme.of(
+          context,
+        ).colorScheme.primary.withValues(alpha: 0.5),
+        inactiveThumbColor: Theme.of(context).colorScheme.secondary,
+        inactiveTrackColor: Theme.of(
+          context,
+        ).colorScheme.secondary.withValues(alpha: 0.5),
+        thumbIcon: WidgetStateProperty.resolveWith<Icon?>((states) {
+          if (states.contains(WidgetState.selected)) {
+            return Icon(
+              Icons.dark_mode,
+              color: Theme.of(context).colorScheme.onPrimary,
+              size: 16,
+            );
+          }
+          return Icon(
+            Icons.light_mode,
+            color: Theme.of(context).colorScheme.onSecondary,
+            size: 16,
+          );
+        }),
+      ),
     );
   }
 }
